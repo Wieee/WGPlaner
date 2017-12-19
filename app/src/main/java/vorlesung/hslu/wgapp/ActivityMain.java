0b64fecb23e731fa32b50a05cd22fa2afdb41c4b
lokaar <32634053+lokaar@users.noreply.github.com>
2017-12-19 12:50:12 +0100

    bug fixing

--- app/src/main/java/vorlesung/hslu/wgapp/ActivityMain.java
+++ app/src/main/java/vorlesung/hslu/wgapp/ActivityMain.java
@@ -68,17 +68,11 @@ public class ActivityMain extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        //Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
         return super.onOptionsItemSelected(item);
     }
 
@@ -114,7 +108,3 @@ public class ActivityMain extends AppCompatActivity implements NavigationView.On
         return true;
     }
 }
-
-
-
-
